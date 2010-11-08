d67feb92193664609a68598d0afee1fd7b49842d
Marcio da Silva <marciottlg@gmail.com>
2010-11-08 19:40:33 -0200

    pep8

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render_to_response
 from libs.utils import RequiredGroup
-   
+
+
 @RequiredGroup('admuser_users')
 def index(request):
     return render_to_response('layout.html')
+
+
+def tab_dispatcher(request, **kw):
+    return getattr(TabContainer, kw['tab'])(request)
+
 
 class TabContainer:
     '''holds tab views'''
@@ -12,6 +18,3 @@ class TabContainer:
     @RequiredGroup('admuser_users')
     def add_user(request):
         return render_to_response('tabs/add_user.html')
-
-def tab_dispatcher(request, **kw):
-    return getattr(TabContainer, kw['tab'])(request)
