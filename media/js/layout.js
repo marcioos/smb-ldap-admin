e8e72bac2d53a94234eaa5bf8cdbc8e61e6656e6
Marcio da Silva <marciottlg@gmail.com>
2010-11-10 11:36:29 -0200

    css

--- media/js/layout.js
+++ media/js/layout.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $("#accordion").accordion();
-    $("#tab_content").css('height', $("#accordion").css('height'));
+    $("#main_content").css('height', $("#accordion").css('height'));
     $(".accordion_link").click(function() {
         $.ajax({
             url: $(this).attr('id'),
             type: 'get',
             success: function(data) {
-                $("#tab_content").html(data)
+                $("#main_content").html(data)
             },
             dataType: 'text'
         });
--- urls.py
+++ urls.py
@@ -1,20 +1,7 @@
 from django.conf.urls.defaults import *
 from frontpage.views import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Example:
-    # (r'^admuser/', include('admuser.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-
     (r'^$', index),
     (r'^(?P<tab>.+)$', tab_dispatcher),
 )
